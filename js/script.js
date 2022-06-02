c6701ebdb4fb9a0694c01ee58fc122c1187dd179
Dhavala-23 <101098856+Dhavala-23@users.noreply.github.com>
2022-06-02 20:33:59 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if (principal == "") { 
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    } 
+    else {
+        if (principal <= 0) { 
+            alert("Enter a positive number");
+            document.getElementById("principal").focus();
+            return false;
+        }
+    }
+
+    var HDB = new Date().getFullYear(); 
+    var future = parseInt(HDB, 10) + parseInt(years, 10);
+    var interest = principal * years * rate / 100;
+    var answer = "If you deposit <mark>" + principal + "</mark>,<br> at an interest rate of <mark>" + rate + "</mark> % <br> You will receive an amount of <mark>" + interest + ",</mark><br> in the year <mark>" + future + "</mark>";
+    document.getElementById("answer").innerHTML = answer;
+}
+
+function showVal(newVal) { 
+    document.getElementById("showRate").innerHTML = newVal + '%';
+}
